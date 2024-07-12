fb37496e8e2a45f2c04a1899d533d7614f8dd83d
Emilce Romano <romano.emilce.belen@gmail.com>
2024-07-12 00:47:47 -0300

    subiendo js

---
+++ js/api.js
@@ -0,0 +1,39 @@
+
+const listarProductos = async () => {
+    const conexion = await fetch("http://localhost:3000/productos");
+    const conexionConvertida =  conexion.json();
+    return conexionConvertida;
+};
+
+async function  enviarProducto(nombre, precio, imagen) {
+        return await fetch("http://localhost:3000/productos", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ 
+            nombre:nombre, 
+            precio:precio, 
+            imagen:imagen 
+        })
+    });
+};
+
+const borrarProducto = async (id) => {
+    try{
+        const res= await fetch(`http://localhost:3000/productos/${id}`, {
+            method: "DELETE"
+        });
+        return await res.json();
+    } catch(err) {
+        return console.log(err);
+    }
+};
+
+
+
+//export 
+export const conexionApi = {
+    listarProductos,
+    enviarProducto,
+    borrarProducto,
+};
+
